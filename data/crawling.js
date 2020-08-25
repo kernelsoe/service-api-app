a9fb21f7e6bb7f3a04e9c11bdafb56283f826053
Kernel Soe <kernelsoe@gmail.com>
2020-08-25 14:37:59 +0900

    update

--- data/crawling.js
+++ data/crawling.js
@@ -1,18 +1,20 @@
 const { payloads } = require('./payloads')
 const Axios = require('axios')
+const { sleep } = require('../lib/sh')
 
 // const api = 'http://localhost:5000'
 const api = 'https://compass-sh.azurewebsites.net'
 
 async function run () {
   for (let i = 0; i < payloads.length; i++) {
+    await sleep(1000)
     const item = payloads[i]
 
     const { data } = await Axios.post(`${api}/sh/crawl`, {
       site: item.root
     })
 
-    console.log(data)
+    console.log(`🚀 ${item.root} `, data)
   }
 }
 
--- data/payloads.js
+++ data/payloads.js
@@ -1,19 +1,196 @@
+const { json } = require("express");
+
 module.exports = {
   payloads: [
+    // {
+    //   root: "https://arkwright.github.io/",
+    //   // pubDateTag: "time",
+    //   payloads: [
+    //     {
+    //       url: "https://arkwright.github.io/",
+    //       q: "li a"
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "http://paulgraham.com",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://www.ribbonfarm.com/",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://mxb.dev/",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://www.zachleat.com/",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://overreacted.io/",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://adamwathan.me/",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://macwright.com",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://trivial.observer",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://fzakaria.com",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://lemire.me",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://zainrizvi.io",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://invertedpassion.com",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://bitwalker.org",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://robert.winter.ink",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "https://deploy.live",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
     {
-      root: "https://arkwright.github.io/",
-      // pubDateTag: "time",
+      root: "https://drewdevault.com",
       payloads: [
         {
-          url: "https://arkwright.github.io/",
-          q: "li a"
+          url: "",
+          q: ""
+        }
+      ]
+    },
+    {
+      root: "https://ptimofeev.com",
+      payloads: [
+        {
+          url: "",
+          q: ""
         }
       ]
     },
     // {
     //   root: "",
-    //   pubDateTag: "",
-    //   payloads: []
-    // }
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
+    // {
+    //   root: "",
+    //   payloads: [
+    //     {
+    //       url: "",
+    //       q: ""
+    //     }
+    //   ]
+    // },
   ]
 }
