2a8582fc77e67075de823a3c50a6f6e844d73ec5
Kernel Soe <kernelsoe@gmail.com>
2020-08-25 11:59:18 +0900

    more fixes ...

--- data/registeration.js
+++ data/registeration.js
@@ -1,7 +1,8 @@
 const { payloads } = require('./payloads')
 const Axios = require('axios')
 
-const api = 'http://localhost:5000'
+// const api = 'http://localhost:5000'
+const api = 'https://compass-sh.azurewebsites.net'
 
 async function run () {
   for (let i = 0; i < payloads.length; i++) {
